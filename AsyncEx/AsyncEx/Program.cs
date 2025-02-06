a2c6af6c0c4c7c1330e5e45a8c9aa2598439c4d0
NicholasBrugnano <145580079+NicholasBrugnano@users.noreply.github.com>
2025-02-05 19:59:26 -0500

    Add files via upload

---
+++ AsyncEx/AsyncEx/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace AsyncEx
+{
+    internal class Program
+    {
+        private static HttpClient _httpClient = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Entering main...");
+            GetExampleDotCom();
+            Console.WriteLine("------------------------------");
+            await GetExampleDotComAsync();
+            Console.WriteLine("Do some work.");
+            Console.WriteLine("Do some more work.");
+            Console.WriteLine("So... you come here often?");
+        }
+
+        private static void GetExampleDotCom()
+        {
+            Console.WriteLine("IO: example.com task started");
+            var result = "website content :)";
+
+            Console.WriteLine("IO: Printing the first few chars of the web page...");
+            Console.WriteLine(result);
+        }
+
+        private static async Task GetExampleDotComAsync()
+        {
+            Console.WriteLine("IO: example.com task started");
+            var result = await _httpClient.GetStringAsync("https://example.com/");
+
+            Console.WriteLine("IO: Printing the first few chars of the web page...");
+            Console.WriteLine(result.Substring(0,50));
+        }
+    }
+}
