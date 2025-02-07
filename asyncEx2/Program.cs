72def940ccc67add355fad555a1b471962d6b2c6
NicholasBrugnano <145580079+NicholasBrugnano@users.noreply.github.com>
2025-02-06 19:50:20 -0500

    Add files via upload

---
+++ asyncEx2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace asyncEx2
+{
+    internal class Program
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("wega");
+            try
+            {
+                await GetSiteLength();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Main 'Task' catch: {ex.Message}");
+            }
+
+            try
+            {
+                GetSiteVoidLength();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Main 'Task' catch: {ex.Message}");
+            }
+            Console.ReadLine();
+            //Console.WriteLine("RAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+        }
+
+        private static async Task GetSiteLength()
+        {
+            var SiteList = new List<string> { "yahoo","google","msn","reddit" };
+            var Wega = new List<string> { "yahoo", "google", "msn", "fandom" };
+
+            foreach (var site in SiteList)
+            {
+                var task = _httpClient.GetStringAsync($"http://{site}.com");
+                await task;
+                Console.WriteLine($"{site} content length is {task.Result.Length}");
+            }
+        }
+
+        private static async void GetSiteVoidLength()
+        {
+            var SiteList = new List<string> { "yahoo", "google", "msn", "reddit" };
+            var Wega = new List<string> { "yahoo", "google", "msn", "fandom" };
+
+            try
+            {
+                foreach (var site in SiteList)
+                {
+                    var task = _httpClient.GetStringAsync($"http://{site}.com");
+                    await task;
+                    Console.WriteLine($"{site} content length is {task.Result.Length}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Site method 'void' catch: {ex.Message}");
+            }
+        }
+    }
+}
