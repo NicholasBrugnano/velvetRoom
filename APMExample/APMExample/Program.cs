b8a3bc043fc561ca229c2808dc0734ef0c5c6ca6
NicholasBrugnano <145580079+NicholasBrugnano@users.noreply.github.com>
2025-02-26 20:01:03 -0500

    note to self: actually finish it over the weekend

---
+++ APMExample/APMExample/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace APMExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\TestData";
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            var files = directoryInfo.GetFiles("*.*", SearchOption.AllDirectories);
+
+            Console.WriteLine($"{files.Length} files found.");
+
+            var buffer = new byte[100000];
+
+            foreach (var file in files) {
+                var stream = file.Open(FileMode.Open,FileAccess.Read);
+
+                stream.BeginRead(buffer, 0, buffer.Length, handleRead,stream);
+            }
+        }
+
+        private static void handleRead(IAsyncResult result)
+        {
+            var fileStream = (FileStream)result.AsyncState;
+            var bytesRead = fileStream.EndRead(result);
+
+            Console.WriteLine($"Read {bytesRead} bytes from file {fileStream.Name}.");
+        }
+    }
+}
