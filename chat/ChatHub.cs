43d489a4a90980b25ae49af5e81e74311f6c4ac1
NicholasBrugnano <145580079+NicholasBrugnano@users.noreply.github.com>
2025-03-12 20:14:13 -0400

    Add files via upload

---
+++ chat/ChatHub.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+using Week08_1_SignalREx.Models;
+
+namespace Week08_1_SignalREx
+{
+    public class ChatHub : Hub
+    {
+        private readonly ConcurrentDictionary<string, ChatRoom> chatRooms = new ConcurrentDictionary<string, ChatRoom>();
+        private readonly ConcurrentDictionary<string, string> userNames = new ConcurrentDictionary<string, string>();
+        private readonly AppDbContext appDbContext;
+
+        public ChatHub(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+        public async Task SendAllMessage(string userName, string textMessage)
+        {
+            var message = new ChatMessage
+            {
+                UserName = userName,
+                Message = textMessage,
+                TimeStamp = DateTimeOffset.Now
+            };
+
+            await Clients.All.SendAsync("ReceiveMessage", message.UserName, message.TimeStamp, message.Message);
+
+        }
+
+        public async Task JoinRoom(string roomName)
+        {
+            roomName = roomName.ToLower();
+
+            string currentConnectionId = Context.ConnectionId;
+
+            if (!chatRooms.ContainsKey(roomName))
+            {
+                //Creates a new room if one doesn't already exist.
+                ChatRoom newRoom = new ChatRoom()
+                {
+                    RoomName = roomName,
+                    ConnectionIds = new List<string>()
+                };
+
+                //Joins said room.
+                newRoom.ConnectionIds.Add(currentConnectionId);
+                chatRooms.TryAdd(roomName, newRoom);
+            }
+            else
+            {
+                //Gets a room. Takes two steps because the Internet is the work of Satan.
+                ChatRoom existingRoom = new ChatRoom();
+                chatRooms.TryGetValue(roomName, out existingRoom);
+
+                //Joins the room.
+                existingRoom.ConnectionIds.Add(currentConnectionId);
+
+                //Updates the dictionary object because now there's a new ConnectionId
+                chatRooms.TryAdd(roomName, existingRoom);
+            }
+            await Groups.AddToGroupAsync(currentConnectionId, roomName);
+            await Clients.Caller.SendAsync("ReceiveMessage", "Chat Hub", DateTimeOffset.Now, $"Joined room: {roomName}");
+        }
+
+        public async Task SendMessage(string roomName, string userName, string textMessage)
+        {
+            if (!userNames.ContainsKey(Context.ConnectionId))
+            {
+                userNames.TryAdd(Context.ConnectionId,userName);
+            }
+
+            var message = new ChatMessage
+            {
+                UserName = userName,
+                Message = textMessage,
+                TimeStamp = DateTimeOffset.Now
+            };
+
+            await Clients.Group(roomName.ToLower()).SendAsync("ReceiveMessage", message.UserName, message.TimeStamp, message.Message);
+
+
+        }
+    }
+}
---
+++ chat/wwwroot/js/site.js
@@ -0,0 +1,63 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+var userName;
+var roomName;
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on('ReceiveMessage', displayMessage);
+connection.start();
+
+var msgForm = document.forms.msgForm;
+
+msgForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var userMessage = document.getElementById('userMessage');
+    var text = userMessage.value;
+    userMessage.value = "";
+    userName = document.getElementById('username').value;
+    roomName = document.getElementById('roomName').value;
+    sendMessage(userName, text);
+});
+
+function sendMessage(userName, message) {
+    if (message && message.length) {
+        if (userName == "GASTER") {
+            //added an gaster egg
+            connection.invoke("SendAllMessage", userName, message);
+        } else {
+            connection.invoke("SendMessage", roomName, userName, message);
+        }
+    }
+}
+
+
+function displayMessage(name, time, message) {
+    var friendlyTime = moment(time).format('H:mm:ss');
+
+    var userLi = document.createElement('li');
+    userLi.className = 'userLi list-group-item';
+    userLi.textContent = friendlyTime + ", " + name + " says:";
+
+    var messageLi = document.createElement('li');
+    messageLi.className = 'messageLi list-group-item pl-5';
+    messageLi.textContent = message;
+
+    var chatHistoryUl = document.getElementById('chatHistory');
+    chatHistoryUl.appendChild(userLi);
+    chatHistoryUl.appendChild(messageLi);
+
+    $('#chatHistory').animate({ scrollTop: $('#chatHistory').prop('scrollHeight') }, 50)
+
+}
+
+document.getElementById("btnJoin").addEventListener('click', function (e) {
+    e.preventDefault();
+    var roomName = document.getElementById("roomName").value;
+    if (roomName && roomName.length) {
+        document.getElementById('btnJoin').disabled = true; //Could probably add a disconnect button to re-enable, but I'm lazy.
+        connection.invoke("JoinRoom", roomName);
+    }
+})
