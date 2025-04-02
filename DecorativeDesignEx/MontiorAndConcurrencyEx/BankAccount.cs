9c4c09c8fad547a1f229204dc4c06423dbd749c9
NicholasBrugnano <145580079+NicholasBrugnano@users.noreply.github.com>
2025-04-02 19:58:00 -0400

    didn't finish it, was too mad about not being allowed to be excited about mario kart world

---
+++ DecorativeDesignEx/MontiorAndConcurrencyEx/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontiorAndConcurrencyEx
+{
+    public class BankAccount
+    {
+        public double Balance { get; set; }
+        public void Deposit(double deposit)
+        {
+            this.Balance += deposit;
+            Console.WriteLine($"Deposited ${deposit}. Current balance: ${Balance}");
+        }
+        public void Withdrawl(double withdrawl)
+        {
+            if (withdrawl > this.Balance)
+            {
+                Console.WriteLine($"Insufficient funds. Current balance: ${Balance}. Attempted withdrawl: ${withdrawl}.");
+            }
+            else
+            {
+                this.Balance -= withdrawl;
+                Console.WriteLine($"Withdrawn ${withdrawl}. Current balance: ${Balance}");
+            }
+        }
+    }
+}
---
+++ DecorativeDesignEx/MontiorAndConcurrencyEx/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MontiorAndConcurrencyEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount1 = new BankAccount();
+            BankAccount bankAccount2 = new BankAccount();
+
+            Console.WriteLine("Testing single thread.");
+            bankAccount1.Deposit(500);
+            bankAccount1.Withdrawl(250);
+            bankAccount1.Withdrawl(400);
+
+            Console.WriteLine("Testing multi-thread.");
+            bankAccount2.Deposit(500);
+            Parallel.Invoke(() =>
+            {
+                bankAccount2.Withdrawl(250);
+            });
+            Parallel.Invoke(() =>
+            {
+                bankAccount2.Withdrawl(400);
+            });
+        }
+    }
+}
